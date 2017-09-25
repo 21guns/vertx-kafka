2b3105b2c0f1011bed72df6fb780675798e8dc8b
jliu <liujun_0810@sohu.com>
2017-09-25 17:16:55 +0800

    issue:add aync deploy

--- src/main/java/io/vertx/starter/MainVerticle.java
+++ src/main/java/io/vertx/starter/MainVerticle.java
@@ -4,30 +4,34 @@ import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
 import io.vertx.core.Vertx;
-import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainVerticle extends AbstractVerticle {
   private static final Logger LOGGER = LoggerFactory.getLogger(MainVerticle.class);
 
-  private Map<String, JsonObject> products = new HashMap<>();
-
   @Override
   public void start(Future<Void> fut) {
-    LOGGER.info("init");
-    DeploymentOptions options = new DeploymentOptions().setWorker(true);
-    options.setInstances(5);
-
     Future<String> dbVerticleDeployment = Future.future();  // <1>
     vertx.deployVerticle(new HttpServerVerticle(), dbVerticleDeployment.completer());  // <2>
-    vertx.deployVerticle("io.vertx.starter.KafkaVerticle", options, dbVerticleDeployment.completer());  // <2>
+
+    dbVerticleDeployment.compose( id -> {
+      Future<String> httpVerticleDeployment = Future.future();
+      vertx.deployVerticle(
+        "io.vertx.starter.KafkaVerticle",  // <4>
+        new DeploymentOptions().setWorker(true).setInstances(5),    // <5>
+        httpVerticleDeployment.completer());
+
+      return httpVerticleDeployment;  // <6>
+    }).setHandler(ar -> {   // <7>
+      if (ar.succeeded()) {
+        fut.complete();
+      } else {
+        fut.fail(ar.cause());
+      }
+    });
+
   }
-
-
 
 
   public static void main(String[] args) {
